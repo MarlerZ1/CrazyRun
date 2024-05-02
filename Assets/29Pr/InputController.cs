a40b45864def45c2ca3f9e833d34900a11324a4a
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-05-02 12:40:23 +0300

    Add right swipe

--- Assets/29Pr/InputController.cs
+++ Assets/29Pr/InputController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float _startX;
+    private float _startY;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                _startX = touch.position.x;
+                _startY = touch.position.y;
+               // print("Start pos: " + _startX + " " + _startY);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+               // print("End pos: " + touch.position.x + " " + touch.position.y);
+                if (touch.position.x - _startX >= 100 && Mathf.Abs(touch.position.y - _startY) <= 50)
+                    print("Right Swipe");
+            }
+        }
+
     }
 }
